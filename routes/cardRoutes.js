7bb0c55e9eb11cccc922f5c6ad8ad0d8e4a4bddb
Nurik <nurzhigit.turman@narxoz.kz>
2025-03-24 04:08:00 +0500

    Add logic for Card

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -120,4 +120,61 @@ router.post("/upload", upload.single("image"), (req, res) => {
   res.json({ imageUrl: `/uploadImages/${req.file.filename}` });
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const card = await Card.findById(req.params.id);
+    if (!card) return res.status(404).send("Карточка не найдена");
+    res.json(card);
+  } catch (err) {
+    res.status(500).send("Ошибка сервера");
+  }
+});
+
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(req.params.id, { isPremium: req.body.isPremium }, { new: true });
+    res.json(card);
+  } catch (err) {
+    res.status(500).send("Ошибка обновления");
+  }
+});
+
+router.patch("/:id/view", async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(req.params.id, { $inc: { views: 1 } }, { new: true });
+    res.json(card);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка обновления просмотров" });
+  }
+});
+
+router.patch("/:id/like", async (req, res) => {
+  try {
+    const card = await Card.findById(req.params.id);
+    card.isFavorite = !card.isFavorite;
+    card.likes += card.isFavorite ? 1 : -1;
+    await card.save();
+    res.json(card);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка обновления лайков" });
+  }
+});
+
+router.patch("/:id/rate", async (req, res) => {
+  try {
+    const { rating } = req.body;
+    const card = await Card.findById(req.params.id);
+
+    // Добавляем новый рейтинг
+    card.rating = (card.rating * card.views + rating) / (card.views + 1);
+    
+    await card.save();
+    res.json(card);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка установки рейтинга" });
+  }
+});
+
+
 module.exports = router;
